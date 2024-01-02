8c99ee202fd5b7c33ce469c5b71cacb78368d670
RaquelD-B <154616671+RaquelD-B@users.noreply.github.com>
2024-01-02 09:23:19 -0300

    Added API functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#currentTemperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  city.innerHTML = response.data.city;
+}
+function showTemperature(city) {
+  let apiKey = "adf0eeed55ed6d4256b9b3ft0e49cc9o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherData);
+}
 function searchCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-Input");
   let city = document.querySelector("#city");
-  city.innerHTML = cityInput.value;
+
+  showTemperature(cityInput.value);
 }
 
 let cityElement = document.querySelector("#city-search-form");
 cityElement.addEventListener("submit", searchCity);
+
+showTemperature("Asuncion");
